0463f51d4f0b37876d58c9fbfad6b002c4fbf6be
Rachel Lee <59715635+rlee9109@users.noreply.github.com>
2025-01-28 04:35:54 +0000

    changes to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,18 +1,20 @@
 ###
 # Helper methods for API Client
-#
-# DTCC Star Coders Year 1 Session 2
+# API sample client for OMDB API: http://omdbapi.com/
 ###
+
 
 import requests
 import json
 
+apiKey='3e3ba345'
 
 # Helper method to parse the different critic ratings input
-def parse_critics_score(critic_scores) -> float:
+def parse_critics_score(critic_scores: list[str]) -> float:
+
   num_of_ratings = len(critic_scores)
   total_score = 0.0
-
+ 
   for critic_rating in critic_scores:
     source = critic_rating['Source']
     value = critic_rating['Value']
@@ -30,9 +32,11 @@ def parse_critics_score(critic_scores) -> float:
 
   return total_score
 
-
 # Helper method to print the values of our results in a readable format
-def print_results(responseBody):
+def print_results(responseBody: dict) -> None:
+  """
+  Prints the contents of an omdbapi response
+  """
   # print (responseBody)
   print("\nTitle of the movie: ", responseBody['Title'])
   print("Year: ", responseBody['Year'])
@@ -40,24 +44,26 @@ def print_results(responseBody):
   print("Genres: ", responseBody['Genre'])
   print("Critic Scores", responseBody['Ratings'], "\n")
 
-
 # Helper method to call API
-def get_info_from_api(title, year):
-  apiKey = '3e3ba345'
+def get_info_from_api(title: str, year: str) -> dict:
+  """
+  Return a dictionary containing the API response from an omdbapi search for a title and year
+  Raises Exception if there is no response
+  """
   url = 'http://www.omdbapi.com/'
-  headers = {'Accept': "application/json"}
-  single_search_payload = {'t': title, 'y': year, 'apiKey': apiKey}
+  headers = {
+    'Accept': 'application/json'
+  }
+  single_search_payload = {'i': 'tt3896198', 'apikey': apiKey}
   # multiple_search_payload = {'s': title, 'type': media_type, 'apiKey': apiKey}
-
+ 
   r = requests.get(url=url, params=single_search_payload, headers=headers)
   responseBody = json.loads(r.text)
-
   if responseBody['Response'] == "False":
     print(responseBody['Error'])
     raise Exception("Error in the search")
   else:
-    #print_results(responseBody)
+    print_results(responseBody)
     return responseBody
-
-
+   
 # ******************************************************
